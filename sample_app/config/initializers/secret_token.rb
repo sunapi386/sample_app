8e143f51ff4e7e96f0e78ebbd47a4c2ba901c1d5
jassun <jassun@blackberry.com>
2013-11-27 18:53:24 -0500

    init repo

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '663df734aeb4ffffb8e19b32419b71538b14e0bd8551285dca56b1866e11e1153f0bb0d286351a2385995804c9246055733ba3fa97b8842054d675c85b8e78bc'
